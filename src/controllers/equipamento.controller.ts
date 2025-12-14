180572c8b1f63c67bdfd287527de243fe00d0ce1
marciojunior11 <marcio_ale1@hotmail.com>
2025-12-14 20:25:35 -0300

    feat: crud equipamento

--- src/controllers/equipamento.controller.ts
+++ src/controllers/equipamento.controller.ts
@@ -1,3 +1,5 @@
+import { prisma } from "@/lib/prisma";
+import { IEquipamento } from "@/models/equipamento.model";
 import EquipamentoService from "@/services/equipamento.service";
 import { Request, Response } from "express";
 
@@ -8,5 +10,36 @@ export default class EquipamentoController {
         const equipamentos = await service.findMany();
 
         return res.json(equipamentos);
+    };
+
+    createEquipamento = async (req: Request, res: Response) => {
+        const data: IEquipamento = req.body;
+
+        const result = await service.create({
+            data: { nome: data.nome }
+        });
+
+        return res.json(result);
+    };
+
+    updateEquipamento = async (req: Request, res: Response) => {
+        const data: IEquipamento = req.body;
+
+        const result = await service.update({
+            data: { nome: data.nome },
+            where: { id: data.id }
+        });
+
+        return res.json(result);
+    };
+
+    deleteEquipamento = async (req: Request, res: Response) => {
+        const id = req.body;
+
+        const result = await service.delete({
+            where: { id: id }
+        });
+
+        return res.json(result);
     };
 }
--- src/routes/equipamento.routes.ts
+++ src/routes/equipamento.routes.ts
@@ -6,5 +6,8 @@ const router = Router();
 const controller = new EquipamentoController();
 
 router.get("/equipamentos", controller.getEquipamentos);
+router.post("/equipamentos", controller.createEquipamento);
+router.put("/equipamentos", controller.createEquipamento);
+router.delete("/equipamentos", controller.createEquipamento);
 
 export default router;
